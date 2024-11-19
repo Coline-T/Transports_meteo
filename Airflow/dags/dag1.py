e068088dd777926448d1b24a2846e918637ba477
Coline-T <coline.treille@free.fr>
2024-11-19 23:44:24 +0100

    dag

--- Airflow/dags/dag1.py
+++ Airflow/dags/dag1.py
@@ -1,14 +1,14 @@
 # Import necessary libraries
-from Airflow import DAG
-from Airflow.operators.python_operator import PythonOperator
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
- 
+
 import subprocess
  
 # Define a function to run the Python script
 def run_trafic_api():
     # Path to your Python file
-    script_path = 'C:\Users\cocop\Desktop\SUP DE VINCI\Entrepôt de données\Transports_meteo\data_collection\getAPI.py'
+    script_path = 'C:\\Users\cocop\Desktop\SUP DE VINCI\Entrepôt de données\Transports_meteo\data_collection\getAPI.py'
     # Using subprocess to run the Python script
     try:
         subprocess.run(['python', script_path], check=True)
@@ -18,7 +18,7 @@ def run_trafic_api():
  
 # Define the DAG
 dag = DAG(
-    'Conso_API_dag',                 # DAG ID
+    'dag1',                 # DAG ID
     description='get data from the trafic API and load it in MongoDB',   # Description
     schedule_interval='0 */1 * * *',               # Schedule interval (runs once per hour) '@daily'
     start_date=datetime(2024, 1, 1),           # Start date (start running from this date)
