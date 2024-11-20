60728e3414b89fc681eaab8eabba16a39c91f335
Solenn-C <solenn.coulon17@gmail.com>
2024-11-20 10:01:34 +0100

    API météo

--- airflow-docker/dags/Dag.py
+++ airflow-docker/dags/Dag.py
@@ -1,39 +1,33 @@
+# Import necessary libraries
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
+
 import subprocess
 
-# Définir les arguments par défaut du DAG
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2024, 11, 1),  # Ajustez la date de début
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
-}
 
-# Définir le DAG
-dag = DAG(
-    'meteo_kafka_dag',
-    default_args=default_args,
-    description='Température météo et humidité',
-    schedule_interval='0 */3 * * *',  # Exécution toutes les 8 heures
-)
-
-
-# Fonction pour exécuter le script sentiment_analysis.py
-def run_meteo():
+# Define a function to run the Python script
+def run_meteo_api():
+    # Path to your Python file
+    script_path = 'C:/Users/solen/PycharmProjects/Transports_meteo/ENV/API_météo.py'
+    # Using subprocess to run the Python script
     try:
-        subprocess.run(['python3',
-                        'C:/Users/solen/PycharmProjects/Transports_meteo/ENV/API_météo.py'],
-                       check=True)
+        subprocess.run(['python', script_path], check=True)
     except subprocess.CalledProcessError as e:
         print(f"Erreur lors de l'exécution du script : {e}")
 
 
-# Définir la tâche pour exécuter le script Python
-run_meteo_task = PythonOperator(
-    task_id='run_meteo',
-    python_callable=run_meteo,
-    dag=dag,
+# Define the DAG
+dag = DAG(
+    'Conso_API_dag',  # DAG ID
+    description='get data from OpenWeather API and load it in MongoDB',  # Description
+    schedule_interval='0 0 */5 * *',  # Exécute tous les 5 jours à minuit
+    start_date = datetime.today(),
+    catchup=False  # Whether to backfill missing DAG runs
+)
+
+run_meteo_api_task = PythonOperator(
+    task_id='run_meteo_api',
+    python_callable=run_meteo_api,  # Function to run
+    dag=dag
 )
